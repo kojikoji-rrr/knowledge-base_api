5f07a091256f744be136e5efbd3048e3d0ab64dc
kojikoji-rrr <>
2026-05-24 19:34:10 +0900

    start project

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+type Bindings = {
+  APP_ENV: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.use(
+  '/*',
+  async (c, next) => {
+    const corsMiddleware = cors({
+      origin: c.env.APP_ENV === 'production' ? 'https://knowledge-base.pages.dev' : '*',
+      allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    })
+    return corsMiddleware(c, next)
+  }
+)
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/hello', (c) => {
+  return c.json({
+    message: '通信できてます！'
+  })
+})
+
+export default app
